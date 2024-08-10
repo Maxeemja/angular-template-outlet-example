d06bc220c7808ec3eb7c04b359624387ee27fecb
Max Hrytsia <maxeemja.inc@gmail.com>
2024-08-10 17:26:13 +0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
-import { WeatherWidgetComponent } from './widgets/weather-widget/weather-widget.component';
+import {Component} from '@angular/core';
+import {WeatherWidgetComponent} from './widgets/weather-widget/weather-widget.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [WeatherWidgetComponent],
-  template: `
-    <weather-widget></weather-widget>
-  `,
-  styles: [`
-    :host {
-      height: 100vh;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-  `]
+    selector: 'app-root',
+    standalone: true,
+    imports: [WeatherWidgetComponent],
+    template: `
+        <weather-widget [headerTemplate]="altWidgetHeader"></weather-widget>
+        <ng-template #altWidgetHeader>
+            <div class="alt-header">Today's weather</div>
+        </ng-template>
+    `,
+    styles: [`
+        :host {
+            height: 100vh;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+        .alt-header {
+            font-size: 24px;
+            font-weight: 300;
+            margin-bottom: -6px;
+        }
+    `]
 })
 export class AppComponent {
 }
--- src/app/widgets/weather-widget/weather-widget.component.ts
+++ src/app/widgets/weather-widget/weather-widget.component.ts
@@ -1,28 +1,46 @@
-import { Component, inject } from '@angular/core';
-import { WidgetActions } from '../widget-actions.service';
-import { WidgetState } from '../widget-state.service';
+import {Component, inject, Input, TemplateRef, ViewChild, ViewContainerRef} from '@angular/core';
+import {WidgetActions} from '../widget-actions.service';
+import {WidgetState} from '../widget-state.service';
+import {NgTemplateOutlet} from "@angular/common";
 
 @Component({
-  selector: 'weather-widget',
-  standalone: true,
-  template: `
-    <div class="widget-header">
-      <div class="widget-title">Weather Forecast</div>
-      <div class="widget-sub-title">Current weather in your location</div>
-    </div>
-    <div class="widget-content">
-      <div class="sky-condition">{{ state.data.skyCondition === 'sunny' ? '☀️' : '☁️' }}</div>
-      <div class="temperature">{{state.data.temperature}}°C</div>
-    </div>
-    <div class="widget-actions">
-      <button (click)="actions.reload()">Reload</button>
-      <button (click)="actions.copyData()">Copy Info</button>
-    </div>
-  `,
-  styleUrls: ['./weather-widget.component.css'],
-  providers: [WidgetActions, WidgetState]
+    selector: 'weather-widget',
+    standalone: true,
+    template: `
+        <div class="widget-header">
+            <ng-container [ngTemplateOutlet]=" headerTemplate|| defaultWidgetHeader"></ng-container>
+        </div>
+        <div class="widget-content">
+            <div class="sky-condition">{{ state.data.skyCondition === 'sunny' ? '☀️' : '☁️' }}</div>
+            <div class="temperature">{{ state.data.temperature }}°C</div>
+        </div>
+        <div class="widget-actions">
+            <button (click)="actions.reload()">Reload</button>
+            <button (click)="actions.copyData()">Copy Info</button>
+        </div>
+
+
+        <ng-template #defaultWidgetHeader>
+            <div class="widget-title">Weather Forecast</div>
+            <div class="widget-sub-title">Current weather in your location</div>
+        </ng-template>
+    `,
+    styleUrls: ['./weather-widget.component.css'],
+    imports: [
+        NgTemplateOutlet
+    ],
+    providers: [WidgetActions, WidgetState]
 })
 export class WeatherWidgetComponent {
-  state = inject(WidgetState);
-  actions = inject(WidgetActions);
+    state = inject(WidgetState);
+    actions = inject(WidgetActions);
+
+    @Input() headerTemplate!: TemplateRef<any>
+
+    /*  @ViewChild('container', {read: ViewContainerRef}) container!: ViewContainerRef;
+      @ViewChild('defaultWidgetHeader') headerTemplate!: TemplateRef<any>;
+      
+      ngAfterViewInit() {
+        this.container.createEmbeddedView(this.headerTemplate);
+      }*/
 }
